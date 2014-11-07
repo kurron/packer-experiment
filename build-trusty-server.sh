4be14d8caf7aa7f8179ae0830209945b1b91d82a
Ron Kurr <rkurr@jvmguy.com>
2014-11-06 20:54:34 -0500

    trusty server is working

--- build-trusty-server.sh
+++ build-trusty-server.sh
@@ -5,3 +5,4 @@ export PACKER_LOG_PATH = /tmp
 
 packer validate trusty-x64-server.json
 packer build trusty-x64-server.json
+vagrant box add --force --name packer/trusty-x64-server vagrant/trusty-x64-server.box
