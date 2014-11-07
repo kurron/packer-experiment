9fa27e97a7ee802ae4ba9cfdf6ecae85c3099f8f
Ron Kurr <rkurr@jvmguy.com>
2014-11-06 22:01:12 -0500

    Added destkop builder script

---
+++ build-trusty-desktop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PACKER_LOG=activate
+#export PACKER_LOG_PATH=/tmp
+
+packer validate trusty-x64-desktop.json
+packer build trusty-x64-desktop.json
+vagrant box add --force --name packer/trusty-x64-desktop vagrant/trusty-x64-desktop.box
--- build-trusty-server.sh
+++ build-trusty-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PACKER_LOG = activate
-export PACKER_LOG_PATH = /tmp
+export PACKER_LOG=activate
+#export PACKER_LOG_PATH=/tmp
 
 packer validate trusty-x64-server.json
 packer build trusty-x64-server.json
