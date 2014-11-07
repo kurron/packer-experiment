546aded820e530d87496f98e6523cb7a798d6816
Ron Kurr <rkurr@jvmguy.com>
2014-11-07 13:47:02 -0500

    Added desktop examples

--- ansible/scripts/passwordless-sudo.sh
+++ ansible/scripts/passwordless-sudo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo '%sudo    ALL=(ALL)  NOPASSWD:ALL' >> /etc/sudoers
-echo 'Defaults !requiretty' >> /etc/sudoers
+echo '%sudo    ALL=(ALL)  NOPASSWD:ALL' >> /etc/sudoers.d/passowrdless
+echo 'Defaults !requiretty' >> /etc/sudoers.d/passwordless
+chmod 0400 /etc/sudoers.d/passwordless
 
