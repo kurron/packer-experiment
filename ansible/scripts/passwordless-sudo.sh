1082e0f45229b35c3f5eda7e4cff5d0b7d2e5283
Ron Kurr <kurr@kurron.org>
2014-12-06 11:25:30 -0500

    Fixed typo in sudoers logic

--- ansible/scripts/passwordless-sudo.sh
+++ ansible/scripts/passwordless-sudo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo '%sudo    ALL=(ALL)  NOPASSWD:ALL' >> /etc/sudoers.d/passowrdless
+echo '%sudo    ALL=(ALL)  NOPASSWD:ALL' >> /etc/sudoers.d/passwordless 
 echo 'Defaults !requiretty' >> /etc/sudoers.d/passwordless
-chmod 0400 /etc/sudoers.d/passwordless
+chmod 0400 /etc/sudoers.d/passwordless 
 
