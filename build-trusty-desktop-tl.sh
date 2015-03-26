f08c158c84dd077a0870daecd5b317b044d3fd39
Ron Kurr <kurr@kurron.org>
2015-03-26 19:52:36 -0400

    Adusted for new Ansible pull mechanism

--- build-trusty-desktop-tl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PACKER_LOG=activate
-#export PACKER_LOG_PATH=/tmp
-
-packer validate trusty-x64-desktop.json
-packer build trusty-x64-desktop.json
-vagrant box add --force --name ansible/trusty-x64-desktop vagrant/trusty-x64-desktop.box
-vagrant box list
