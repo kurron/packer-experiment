703cbb7068e9f6f5f3b03cfc29c37e5ee756ab10
Ron Kurr <kurr@kurron.org>
2015-04-15 10:15:09 -0400

    Kept only the Utopic files

--- build-trusty-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PACKER_LOG=activate
-#export PACKER_LOG_PATH=/tmp
-
-packer validate trusty-x64-server.json
-packer build trusty-x64-server.json
-vagrant box add --force --name trusty-x64-server/4.3.26 vagrant/trusty-x64-server.box
-vagrant box list
