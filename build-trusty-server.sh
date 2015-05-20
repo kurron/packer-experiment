8f50d1832a557fd337eff5a77582ab5f501ed741
Ron Kurr <kurr@kurron.org>
2015-05-19 21:34:24 -0400

    Upgrade to newest VirtualBox

--- build-trusty-server.sh
+++ build-trusty-server.sh
@@ -5,5 +5,5 @@ export PACKER_LOG=activate
 
 packer validate trusty-x64-server.json
 packer build trusty-x64-server.json
-vagrant box add --force --name trusty-x64-server/4.3.26 vagrant/trusty-x64-server.box
+vagrant box add --force --name trusty-x64-server/4.3.28 vagrant/trusty-x64-server.box
 vagrant box list
--- publish-boxes.sh
+++ publish-boxes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl --request PUT --user "SVNBuild:Snoopy*09" --header "Accept: application/json" --header "Content-Type: application/octet-stream" --data-binary @vagrant/trusty-x64-server.box http://cd1:81/artifactory/vm-artifacts/vagrant/ubuntu/trusty-x64/4.3.26/trusty-x64-server.box
+curl --request PUT --user "SVNBuild:Snoopy*09" --header "Accept: application/json" --header "Content-Type: application/octet-stream" --data-binary @vagrant/trusty-x64-server.box http://192.168.254.81:81/artifactory/vm-artifacts/vagrant/ubuntu/trusty-x64/4.3.28/trusty-x64-server.box
 
-curl --request PUT --user "SVNBuild:Snoopy*09" --header "Accept: application/json" --header "Content-Type: application/octet-stream" --data-binary @vagrant/trusty-x64-desktop.box http://cd1:81/artifactory/vm-artifacts/vagrant/ubuntu/trusty-x64/4.3.26/trusty-x64-desktop.box
+curl --request PUT --user "SVNBuild:Snoopy*09" --header "Accept: application/json" --header "Content-Type: application/octet-stream" --data-binary @vagrant/trusty-x64-desktop.box http://192.168.254.81:81/artifactory/vm-artifacts/vagrant/ubuntu/trusty-x64/4.3.28/trusty-x64-desktop.box
 
