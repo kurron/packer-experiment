060122085ae9c1946c4243cbb0c0d45b877aa374
Ron Kurr <kurr@kurron.org>
2015-07-16 21:00:21 -0400

    Upgrade to current Packer

--- build-trusty-desktop.sh
+++ build-trusty-desktop.sh
@@ -5,5 +5,5 @@ export PACKER_LOG=activate
 
 packer validate trusty-x64-desktop.json
 packer build trusty-x64-desktop.json
-vagrant box add --force --name trusty-x64-desktop/4.3.28 vagrant/trusty-x64-desktop.box
+vagrant box add --force --name trusty-x64-desktop/5.0.0 vagrant/trusty-x64-desktop.box
 vagrant box list
