199f0066c4adb1fc7bb11835bec67b0ddbf149fe
Ron Kurr <kurr@kurron.org>
2015-08-17 20:05:16 -0400

    Update to VirtualBox 5

--- build-vivid-desktop.sh
+++ build-vivid-desktop.sh
@@ -5,5 +5,5 @@ export PACKER_LOG=activate
 
 packer validate vivid-x64-desktop.json
 packer build vivid-x64-desktop.json
-vagrant box add --force --name vivid-x64-desktop/4.3.26 vagrant/vivid-x64-desktop.box
+vagrant box add --force --name vivid-x64-desktop/5.0.0 vagrant/vivid-x64-desktop.box
 vagrant box list
