4423222033d64d114d5ae501f41070a5da8a343b
Ron Kurr <kurr@kurron.org>
2015-11-13 20:30:22 -0500

    Upgraded the destktop image

--- build-trusty-desktop.sh
+++ build-trusty-desktop.sh
@@ -5,5 +5,5 @@ export PACKER_LOG=activate
 
 packer validate trusty-x64-desktop.json
 packer build trusty-x64-desktop.json
-vagrant box add --force --name trusty-x64-desktop/5.0.8 vagrant/trusty-x64-desktop.box
+vagrant box add --force --name trusty-x64-desktop/5.0.10 vagrant/trusty-x64-desktop.box
 vagrant box list
