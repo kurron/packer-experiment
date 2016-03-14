94ae02766a59c0d4594a7084c61c7d9ad7d75b33
Ron Kurr <kurr@kurron.org>
2016-03-14 13:27:52 -0400

    Removed old shell scripts

--- install-boxes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-vagrant box add --force --name trusty-x64-desktop/4.3.28 vagrant/trusty-x64-desktop.box
-vagrant box add --force --name trusty-x64-server/4.3.28 vagrant/trusty-x64-server.box
-vagrant box list
-
