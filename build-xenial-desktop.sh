f1ee2c8f25528f7d1c0fb6dad3734388e06ded1a
Ron Kurr <kurr@kurron.org>
2016-04-29 20:13:01 -0400

    VirtualBox 5.0.20 desktop box

--- build-xenial-desktop.sh
+++ build-xenial-desktop.sh
@@ -5,5 +5,5 @@ export PACKER_LOG=activate
 
 packer validate xenial-x64-desktop.json
 packer build --force xenial-x64-desktop.json
-vagrant box add --force --name xenial-x64-desktop/5.0.18 vagrant/xenial-x64-desktop.box
+vagrant box add --force --name xenial-x64-desktop/5.0.20 vagrant/xenial-x64-desktop.box
 vagrant box list
