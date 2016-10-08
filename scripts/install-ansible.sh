cfd046670f90beb86a75ced96c5ce46376fd68af
Ron Kurr <kurr@kurron.org>
2016-10-07 23:13:28 -0400

    Working on Xubuntu baked

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -3,7 +3,7 @@
 until sudo apt-get update; do echo "Waiting for apt-get lock"; sleep 5; done
 
 # supposedly, this is the newer way to install pip
-sudo apt-get install -y python-pip python-dev build-essential libssl-dev libffi-dev
+sudo apt-get install -y python-pip python-dev build-essential libssl-dev libffi-dev apt-transport-https
 sudo pip install --upgrade pip
 sudo pip install --upgrade ansible
 sudo pip install --upgrade boto
