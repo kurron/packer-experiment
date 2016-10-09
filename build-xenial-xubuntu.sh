910c34a20ea701c70431836773c842d4a0f1fede
Ron Kurr <kurr@kurron.org>
2016-10-09 12:20:30 -0400

    Chain the xubuntu box into the xubuntu-baked box

--- build-xenial-xubuntu.sh
+++ build-xenial-xubuntu.sh
@@ -5,5 +5,7 @@ export PACKER_LOG=activate
 
 packer validate xenial-x64-xubuntu.json
 packer build --force xenial-x64-xubuntu.json
+mkdir --verbose keep
+mv --force output-virtualbox-iso/xenial-x64-xubuntu.ova keep
 vagrant box add --force --name xenial-x64-xubuntu/5.1.6 vagrant/xenial-x64-xubuntu.box
 vagrant box list
