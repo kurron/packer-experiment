ca2ae6b8add6b8907c3956856fd890616922edd1
Ron Kurr <kurr@kurron.org>
2016-10-31 20:39:21 -0400

    Upgrade to VirtualBox 5.1.8

--- build-centos-gnome-baked.sh
+++ build-centos-gnome-baked.sh
@@ -5,5 +5,5 @@ export PACKER_LOG=activate
 
 packer validate centos-x64-gnome-baked.json
 packer build --force centos-x64-gnome-baked.json
-vagrant box add --force --name centos-x64-gnome-baked/5.1.7 vagrant/centos-x64-gnome-baked.box
+vagrant box add --force --name centos-x64-gnome-baked/5.1.8 vagrant/centos-x64-gnome-baked.box
 vagrant box list
--- scripts/install-ansible-centos.sh
+++ scripts/install-ansible-centos.sh
@@ -14,3 +14,15 @@ sudo pip install --upgrade paramiko
 sudo pip install --upgrade ansible
 sudo pip install --upgrade boto
 
+# Ansible currently breaks on the new docker-py 1.10.0 so we pin it for now.
+# https://github.com/ansible/ansible/issues/17495
+sudo pip install 'docker-py==1.9.0'
+
+# install the required Ansible roles
+sudo ansible-galaxy install --verbose --role-file /tmp/desktop-requirements.yml
+
+# for some reason, this is owned by root so we clear it out for the normal user
+sudo rm -rf /home/vagrant/.ansible
+
+# run the playbook
+ansible-playbook --verbose --inventory-file /tmp/inventory --user vagrant /tmp/desktop.yml
