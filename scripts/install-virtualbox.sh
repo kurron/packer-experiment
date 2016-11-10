d31c42d3edc498af35308004221086582d4b89f9
Ron Kurr <kurr@kurron.org>
2016-11-10 15:28:56 -0500

    Added RHEL Maipo server

--- scripts/install-virtualbox.sh
+++ scripts/install-virtualbox.sh
@@ -4,7 +4,7 @@
 # Making sure everything is updated
 #apt-get update
 #apt-get -y upgrade
-#apt-get -f install 
+#apt-get -f install
 
 # Fedora seems to want this
 export KERN_DIR=/usr/src/kernels/`uname -r`
@@ -14,7 +14,7 @@ VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
 mount -o loop $VBOX_ISO /mnt
 #yes | sh /mnt/VBoxLinuxAdditions.run
-/mnt/VBoxLinuxAdditions.run
+/mnt/VBoxLinuxAdditions.run --nox11
 umount /mnt
 
 #Remove the guest additions ISO so it doesn't take up space in the finished base box:
