59a9a5cc26937bb9d4d24d0180848ec078066c9b
Ron Kurr <kurr@kurron.org>
2016-11-20 10:02:37 -0500

    Alternate GUIs live in an experimental branch now.  Too many files.

--- build-xenial-gnome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PACKER_LOG=activate
-#export PACKER_LOG_PATH=/tmp
-
-packer validate xenial-x64-gnome.json
-packer build --force xenial-x64-gnome.json
-vagrant box add --force --name xenial-x64-gnome/5.1.6 vagrant/xenial-x64-gnome.box
-vagrant box list
--- build-xenial-kubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PACKER_LOG=activate
-#export PACKER_LOG_PATH=/tmp
-
-packer validate xenial-x64-kubuntu.json
-packer build --force xenial-x64-kubuntu.json
-vagrant box add --force --name xenial-x64-kubuntu/5.1.6 vagrant/xenial-x64-kubuntu.box
-vagrant box list
